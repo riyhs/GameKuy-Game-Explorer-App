429e636fb6f615f4a9e2fd917354848f5afdd918
Riyaldi Hasan <riy.2923@gmail.com>
2021-02-18 17:07:14 +0700

    add new module

--- favorite/src/main/java/com/riyaldi/gamekuy/favorite/FavoriteFragment.kt
+++ favorite/src/main/java/com/riyaldi/gamekuy/favorite/FavoriteFragment.kt
@@ -1,11 +1,10 @@
 package com.riyaldi.gamekuy.favorite
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.riyaldi.gamekuy.R
+import androidx.fragment.app.Fragment
 
 class FavoriteFragment : Fragment() {
     override fun onCreateView(
